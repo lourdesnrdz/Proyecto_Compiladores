12cdb5be5a19d71de57bab442565bc4e8cab55ff
lourdesnrdz <lulynavarrete@hotmail.com>
2020-04-26 19:36:18 -0500

    cubo semántico, combinaciones entre tipos

--- semantics_cube.py
+++ semantics_cube.py
@@ -12,164 +12,99 @@ semantic_cube = defaultdict(
 
 #INT 
 
-# suma
+semantic_cube['int']['&']['int'] = 'bool'
+semantic_cube['int']['|']['int'] = 'bool'
+semantic_cube['int']['>']['int'] = 'bool'
+semantic_cube['int']['<']['int'] = 'bool'
+semantic_cube['int']['>=']['int'] = 'bool'
+semantic_cube['int']['<=']['int'] = 'bool'
+semantic_cube['int']['==']['int'] = 'bool'
+semantic_cube['int']['!=']['int'] = 'bool'
 semantic_cube['int']['+']['int'] = 'int'
-semantic_cube['int']['+']['float'] = 'float'
-# semantic_cube['int']['+']['char'] = ''
-
-# resta
 semantic_cube['int']['-']['int'] = 'int'
-semantic_cube['int']['-']['float'] = 'float'
-# semantic_cube['int']['-']['char'] = ''
-
-# multiplicación
 semantic_cube['int']['*']['int'] = 'int'
-semantic_cube['int']['*']['float'] = 'float'
-# semantic_cube['int']['*']['char'] = ''
-
-# division 
 semantic_cube['int']['/']['int'] = 'int'
-semantic_cube['int']['/']['float'] = 'float'
-# semantic_cube['int']['/']['char'] = ''
-
-#FLOAT
-
-# suma
-semantic_cube['float']['+']['int'] = 'float'
-semantic_cube['float']['+']['float'] = 'float'
-# semantic_cube['float']['+']['char'] = ''
-
-# resta
-semantic_cube['float']['-']['int'] = 'float'
-semantic_cube['float']['-']['float'] = 'float'
-# semantic_cube['float']['-']['char'] = ''
-
-# multiplicación
-semantic_cube['float']['*']['int'] = 'float'
-semantic_cube['float']['*']['float'] = 'float'
-# semantic_cube['float']['*']['char'] = ''
-
-# division 
-semantic_cube['float']['/']['int'] = 'float'
-semantic_cube['float']['/']['float'] = 'float'
-# semantic_cube['float']['/']['char'] = ''
-
-#CHAR
-
-# suma
-# semantic_cube['char']['+']['int'] = ''
-# semantic_cube['char']['+']['float'] = ''
-semantic_cube['char']['+']['char'] = 'char'
-
-# resta
-# semantic_cube['char']['-']['int'] = ''
-# semantic_cube['char']['-']['float'] = ''
-# semantic_cube['char']['-']['char'] = ''
-
-# multiplicación
-# semantic_cube['char']['*']['int'] = ''
-# semantic_cube['char']['*']['float'] = ''
-# semantic_cube['char']['*']['char'] = ''
-
-# division 
-# semantic_cube['char']['/']['int'] = ''
-# semantic_cube['char']['/']['float'] = ''
-# semantic_cube['char']['/']['char'] = ''
-
-
-# OPERACIONES CONDICIONALES
-
-# INT
-
-# mayorque
-semantic_cube['int']['>']['int'] = 'int'
-semantic_cube['int']['>']['float'] = 'int'
-# semantic_cube['int']['>']['char'] = ''
-
-# menorque
-semantic_cube['int']['<']['int'] = 'int'
-semantic_cube['int']['<']['float'] = 'int'
-# semantic_cube['int']['<']['char'] = ''
-
-# mayorigual
-semantic_cube['int']['>=']['int'] = 'int'
-semantic_cube['int']['>=']['float'] = 'int'
-# semantic_cube['int']['>=']['char'] = ''
-
-# menorigual
-semantic_cube['int']['<=']['int'] = 'int'
-semantic_cube['int']['<=']['float'] = 'int'
-# semantic_cube['int']['<=']['char'] = ''
-
-# igualigual
-semantic_cube['int']['==']['int'] = 'int'
-semantic_cube['int']['==']['float'] = 'int'
-# semantic_cube['int']['==']['char'] = ''
-
-# diferente
-semantic_cube['int']['!=']['int'] = 'int'
-semantic_cube['int']['!=']['float'] = 'int'
-# semantic_cube['int']['!=']['char'] = ''
+semantic_cube['int']['=']['int'] = 'int'
 
 # FLOAT
 
-# mayorque
-semantic_cube['float']['>']['int'] = 'int'
-semantic_cube['float']['>']['float'] = 'int'
-# semantic_cube['float']['>']['char'] = ''
-
-# menorque
-semantic_cube['float']['<']['int'] = 'int'
-semantic_cube['float']['<']['float'] = 'int'
-# semantic_cube['float']['<']['char'] = ''
-
-# mayorigual
-semantic_cube['float']['>=']['int'] = 'int'
-semantic_cube['float']['>=']['float'] = 'int'
-# semantic_cube['float']['>=']['char'] = ''
-
-# menorigual
-semantic_cube['float']['<=']['int'] = 'int'
-semantic_cube['float']['<=']['float'] = 'int'
-# semantic_cube['float']['<=']['char'] = ''
-
-# igualigual
-semantic_cube['float']['==']['int'] = 'int'
-semantic_cube['float']['==']['float'] = 'int'
-# semantic_cube['float']['==']['char'] = ''
-
-# diferente
-semantic_cube['float']['!=']['int'] = 'int'
-semantic_cube['float']['!=']['float'] = 'int'
-# semantic_cube['float']['!=']['char'] = ''
+semantic_cube['float']['&']['float'] = 'bool'
+semantic_cube['float']['|']['float'] = 'bool'
+semantic_cube['float']['>']['float'] = 'bool'
+semantic_cube['float']['<']['float'] = 'bool'
+semantic_cube['float']['>=']['float'] = 'bool'
+semantic_cube['float']['<=']['float'] = 'bool'
+semantic_cube['float']['==']['float'] = 'bool'
+semantic_cube['float']['!=']['float'] = 'bool'
+semantic_cube['float']['+']['float'] = 'float'
+semantic_cube['float']['-']['float'] = 'float'
+semantic_cube['float']['*']['float'] = 'float'
+semantic_cube['float']['/']['float'] = 'float'
+semantic_cube['float']['=']['float'] = 'float'
 
 # CHAR
-# mayorque
-# semantic_cube['char']['>']['int'] = 'int'
-# semantic_cube['char']['>']['float'] = 'int'
-# semantic_cube['char']['>']['char'] = 'int'
 
-# menorque
-# semantic_cube['char']['<']['int'] = 'int'
-# semantic_cube['char']['<']['float'] = 'int'
-# semantic_cube['char']['<']['char'] = 'int'
+semantic_cube['char']['&']['char'] = 'bool'
+semantic_cube['char']['|']['char'] = 'bool'
+semantic_cube['char']['>']['char'] = 'bool'
+semantic_cube['char']['<']['char'] = 'bool'
+semantic_cube['char']['>=']['char'] = 'bool'
+semantic_cube['char']['<=']['char'] = 'bool'
+semantic_cube['char']['==']['char'] = 'bool'
+semantic_cube['char']['!=']['char'] = 'bool'
+semantic_cube['char']['+']['char'] = 'char'
+# semantic_cube['char']['-']['char'] = 'char'
+# semantic_cube['char']['*']['char'] = 'char'
+# semantic_cube['char']['/']['char'] = 'char'
+semantic_cube['char']['=']['char'] = 'char'
 
-# mayorigual
-# semantic_cube['char']['>=']['int'] = 'int'
-# semantic_cube['char']['>=']['float'] = 'int'
-# semantic_cube['char']['>=']['char'] = 'int'
+# INT FLOAT
 
-# menorigual
-# semantic_cube['char']['<=']['int'] = 'int'
-# semantic_cube['char']['<=']['float'] = 'int'
-# semantic_cube['char']['<=']['char'] = 'int'
+semantic_cube['int']['&']['float'] = semantic_cube['float']['&']['int'] = 'bool'
+semantic_cube['int']['|']['float'] = semantic_cube['float']['|']['int'] = 'bool'
+semantic_cube['int']['>']['float'] = semantic_cube['float']['>']['int'] = 'bool'
+semantic_cube['int']['<']['float'] = semantic_cube['float']['<']['int'] = 'bool'
+semantic_cube['int']['>=']['float'] = semantic_cube['float']['>=']['int'] = 'bool'
+semantic_cube['int']['<=']['float'] = semantic_cube['float']['<=']['int'] = 'bool'
+semantic_cube['int']['==']['float'] = semantic_cube['float']['==']['int'] = 'bool'
+semantic_cube['int']['!=']['float'] = semantic_cube['float']['!=']['int'] = 'bool'
+semantic_cube['int']['+']['float'] = semantic_cube['float']['+']['int'] = 'float'
+semantic_cube['int']['-']['float'] = semantic_cube['float']['-']['int'] = 'float'
+semantic_cube['int']['*']['float'] = semantic_cube['float']['*']['int'] = 'float'
+semantic_cube['int']['/']['float'] = semantic_cube['float']['/']['int'] = 'float'
+semantic_cube['int']['=']['float'] = 'int'
+semantic_cube['float']['=']['int'] = 'float'
 
-# igualigual
-# semantic_cube['char']['==']['int'] = 'int'
-# semantic_cube['char']['==']['float'] = 'int'
-semantic_cube['char']['==']['char'] = 'int'
+# INT CHAR
 
-# diferente
-# semantic_cube['char']['!=']['int'] = 'int'
-# semantic_cube['char']['!=']['float'] = 'int'
-semantic_cube['char']['!=']['char'] = 'int'
+semantic_cube['int']['&']['char'] = semantic_cube['char']['&']['int'] = 'bool'
+semantic_cube['int']['|']['char'] = semantic_cube['char']['|']['int'] = 'bool'
+semantic_cube['int']['>']['char'] = semantic_cube['char']['>']['int'] = 'bool'
+semantic_cube['int']['<']['char'] = semantic_cube['char']['<']['int'] = 'bool'
+semantic_cube['int']['>=']['char'] = semantic_cube['char']['>=']['int'] = 'bool'
+semantic_cube['int']['<=']['char'] = semantic_cube['char']['<=']['int'] = 'bool'
+semantic_cube['int']['==']['char'] = semantic_cube['char']['==']['int'] = 'bool'
+semantic_cube['int']['!=']['char'] = semantic_cube['char']['!=']['int'] = 'bool'
+semantic_cube['int']['+']['char'] = semantic_cube['char']['+']['int'] = 'int'
+semantic_cube['int']['-']['char'] = semantic_cube['char']['-']['int'] = 'int'
+semantic_cube['int']['*']['char'] = semantic_cube['char']['*']['int'] = 'int'
+semantic_cube['int']['/']['char'] = semantic_cube['char']['/']['int'] = 'int'
+semantic_cube['int']['=']['char'] = 'int'
+semantic_cube['char']['=']['int'] = 'char'
+
+# FLOAT CHAR
+
+semantic_cube['float']['&']['char'] = semantic_cube['char']['&']['float'] = 'bool'
+semantic_cube['float']['|']['char'] = semantic_cube['char']['|']['float'] = 'bool'
+semantic_cube['float']['>']['char'] = semantic_cube['char']['>']['float'] = 'bool'
+semantic_cube['float']['<']['char'] = semantic_cube['char']['<']['float'] = 'bool'
+semantic_cube['float']['>=']['char'] = semantic_cube['char']['>=']['float'] = 'bool'
+semantic_cube['float']['<=']['char'] = semantic_cube['char']['<=']['float'] = 'bool'
+semantic_cube['float']['==']['char'] = semantic_cube['char']['==']['float'] = 'bool'
+semantic_cube['float']['!=']['char'] = semantic_cube['char']['!=']['float'] = 'bool'
+semantic_cube['float']['+']['char'] = semantic_cube['char']['+']['float'] = 'float'
+semantic_cube['float']['-']['char'] = semantic_cube['char']['-']['float'] = 'float'
+semantic_cube['float']['*']['char'] = semantic_cube['char']['*']['float'] = 'float'
+semantic_cube['float']['/']['char'] = semantic_cube['char']['/']['float'] = 'float'
+semantic_cube['float']['=']['char'] = 'float'
+semantic_cube['char']['=']['float'] = 'char'
